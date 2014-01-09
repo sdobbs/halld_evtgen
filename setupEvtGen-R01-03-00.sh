e3125942d0c924ddb7e8486329a30bdaa5c60b9a
John Back <J.J.Back@warwick.ac.uk>
2014-01-09 17:11:54 +0000

    Replaced auto-install script to work with this version as well as the latest versions of all external generator packages. Updated README to mention the new CERN-based web pages for Photos++ and Tauola++. In preparation for new tag "1.3.0".

--- setupEvtGen-R01-03-00.sh
+++ setupEvtGen-R01-03-00.sh
@@ -5,25 +5,25 @@ cd EvtGen
 
 INSTALL_BASE=`pwd`
 
-echo Will setup EvtGen R01-02-00 in $INSTALL_BASE
+echo Will setup EvtGen R01-03-00 in $INSTALL_BASE
 
 echo Downloading EvtGen from SVN
-svn export http://svnweb.cern.ch/guest/evtgen/tags/R01-02-00
+svn export http://svnweb.cern.ch/guest/evtgen/tags/R01-03-00
 
 echo Downloading external dependencies
 mkdir -p external
 cd external
 
-wget http://home.thep.lu.se/~torbjorn/pythia8/pythia8176.tgz
-wget http://annapurna.ifj.edu.pl/~tprzedzinski/resources/PHOTOS.3.52/PHOTOS.3.52.tar.gz
-wget http://annapurna.ifj.edu.pl/~tprzedzinski/resources/TAUOLA.1.1.1a/TAUOLA.1.1.1a.tar.gz
+wget http://home.thep.lu.se/~torbjorn/pythia8/pythia8180.tgz
+wget http://photospp.web.cern.ch/photospp/resources/PHOTOS.3.54/PHOTOS.3.54.tar.gz
+wget http://tauolapp.web.cern.ch/tauolapp/resources/TAUOLA.1.1.4/TAUOLA.1.1.4.tar.gz
 wget http://lcgapp.cern.ch/project/simu/HepMC/download/HepMC-2.06.08.tar.gz
 
 echo Extracting external dependencies
-tar -xzf PHOTOS.3.52.tar.gz 
+tar -xzf PHOTOS.3.54.tar.gz 
 tar -xzf HepMC-2.06.08.tar.gz 
-tar -xzf pythia8176.tgz 
-tar -xzf TAUOLA.1.1.1a.tar.gz 
+tar -xzf pythia8180.tgz
+tar -xzf TAUOLA.1.1.4.tar.gz 
 
 echo Installing HepMC in $INSTALL_BASE/external/HepMC/
 mkdir -p HepMC
@@ -32,8 +32,8 @@ cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_BASE/external/HepMC/ $INSTALL_BASE/externa
 make
 make install
 
-echo Installing pythia8 in $INSTALL_BASE/external/pythia8176/
-cd ../pythia8176/
+echo Installing pythia8 in $INSTALL_BASE/external/pythia8180/
+cd ../pythia8180/
 ./configure --with-hepmc=$INSTALL_BASE/external/HepMC/ --with-hepmcversion=2.06.08 --enable-shared
 make
 
@@ -48,10 +48,10 @@ cd ../PHOTOS/
 make
 
 echo Building EvtGen
-cd $INSTALL_BASE/R01-02-00/
-./configure --hepmcdir=$INSTALL_BASE/external/HepMC/ --photosdir=$INSTALL_BASE/external/PHOTOS/ --pythiadir=$INSTALL_BASE/external/pythia8176/ --tauoladir=$INSTALL_BASE/external/TAUOLA/
+cd $INSTALL_BASE/R01-03-00/
+./configure --hepmcdir=$INSTALL_BASE/external/HepMC/ --photosdir=$INSTALL_BASE/external/PHOTOS/ --pythiadir=$INSTALL_BASE/external/pythia8180/ --tauoladir=$INSTALL_BASE/external/TAUOLA/
 make
 
 echo Setup done.
 echo To complete, add the following command to your .bashrc file or run it in your terminal before running any programs that use the EvtGen library:
-echo LD_LIBRARY_PATH=$INSTALL_BASE/external/HepMC/lib:$INSTALL_BASE/external/pythia8176/lib:$INSTALL_BASE/external/PHOTOS/lib:$INSTALL_BASE/external/TAUOLA/lib:$INSTALL_BASE/R01-02-00/lib:\$LD_LIBRARY_PATH
+echo LD_LIBRARY_PATH=$INSTALL_BASE/external/HepMC/lib:$INSTALL_BASE/external/pythia8180/lib:$INSTALL_BASE/external/PHOTOS/lib:$INSTALL_BASE/external/TAUOLA/lib:$INSTALL_BASE/R01-03-00/lib:\$LD_LIBRARY_PATH
