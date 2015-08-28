ae31d80d342c48af8c52a0faee794e3bd6f4b8f4
John Back <J.J.Back@warwick.ac.uk>
2015-08-28 12:33:43 +0000

    Changed the installation and set-up script name to be just setupEvtGen.sh; it uses the VERSION variable to specify the required tag. Added pdf documentation files for the new B0_q -> llll models.

--- setupEvtGen.sh
+++ setupEvtGen.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
+# This script installs EvtGen with all external dependencies. The variable VERSION specifies
+# the tag of EvtGen you want to use. The list of available tags can be found using either the
+# command "svn ls -v http://svn.cern.ch/guest/evtgen/tags" or by going to the url
+# http://svn.cern.ch/guest/evtgen/tags in a web browser. Note that some earlier EvtGen versions
+# will not be compatible with all external depedency versions given below, owing to C++
+# interface differences; see the specific tagged version of the EvtGen/README file for guidance
+
+VERSION=R01-05-00
+
 mkdir -p EvtGen
 cd EvtGen
 
 INSTALL_BASE=`pwd`
 
-echo Will setup EvtGen R01-04-00 in $INSTALL_BASE
+echo Will setup EvtGen $VERSION in $INSTALL_BASE
 
 echo Downloading EvtGen from SVN
-svn export http://svn.cern.ch/guest/evtgen/tags/R01-04-00
+svn export http://svn.cern.ch/guest/evtgen/tags/$VERSION
 
 echo Downloading external dependencies
 mkdir -p external
 cd external
 
+# Minimum required versions of the external packages. Later versions should be OK as well,
+# assuming their C++ interfaces do not change
 wget http://home.thep.lu.se/~torbjorn/pythia8/pythia8180.tgz
 wget http://photospp.web.cern.ch/photospp/resources/PHOTOS.3.54/PHOTOS.3.54.tar.gz
 wget http://tauolapp.web.cern.ch/tauolapp/resources/TAUOLA.1.1.4/TAUOLA.1.1.4.tar.gz
@@ -48,12 +59,12 @@ cd ../PHOTOS/
 make
 
 echo Building EvtGen
-cd $INSTALL_BASE/R01-04-00/
+cd $INSTALL_BASE/$VERSION
 ./configure --hepmcdir=$INSTALL_BASE/external/HepMC/ --photosdir=$INSTALL_BASE/external/PHOTOS/ --pythiadir=$INSTALL_BASE/external/pythia8180/ --tauoladir=$INSTALL_BASE/external/TAUOLA/
 make
 
 echo Setup done.
 echo To complete, add the following command to your .bashrc file or run it in your terminal before running any programs that use the EvtGen library:
-echo LD_LIBRARY_PATH=$INSTALL_BASE/external/HepMC/lib:$INSTALL_BASE/external/pythia8180/lib:$INSTALL_BASE/external/PHOTOS/lib:$INSTALL_BASE/external/TAUOLA/lib:$INSTALL_BASE/R01-04-00/lib:\$LD_LIBRARY_PATH
+echo LD_LIBRARY_PATH=$INSTALL_BASE/external/HepMC/lib:$INSTALL_BASE/external/pythia8180/lib:$INSTALL_BASE/external/PHOTOS/lib:$INSTALL_BASE/external/TAUOLA/lib:$INSTALL_BASE/$VERSION/lib:\$LD_LIBRARY_PATH
 echo Also set the Pythia8 data path:
 echo PYTHIA8DATA=$INSTALL_BASE/external/pythia8180/xmldoc
