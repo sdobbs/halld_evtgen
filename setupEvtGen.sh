db1f5f309bfd5e2872acf2a45751ef47c146c2cf
John Back <J.J.Back@warwick.ac.uk>
2015-09-10 14:49:37 +0000

    Updated setupEvtGen.sh to use the recommended external packages: HepMC 2.06.09, pythia 8.186, Photos++ 3.56 and Tauola++ 1.1.5.
    In the validation directory, added runPhotosTest.sh for testing FSR in
    Upsilon(4S) -> e+ e- decays, and changed the plot comparison scripts to
    use the 2nd directory "oldRootFiles" (which could be a soft-link) for
    including ROOT histograms made from a previous version of EvtGen.

--- setupEvtGen.sh
+++ setupEvtGen.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # This script installs EvtGen with all external dependencies. The variable VERSION specifies
-# the tag of EvtGen you want to use. The list of available tags can be found using either the
+# the tag of EvtGen you want to use. The list of available tags can be found using either the 
 # command "svn ls -v http://svn.cern.ch/guest/evtgen/tags" or by going to the url
 # http://svn.cern.ch/guest/evtgen/tags in a web browser. Note that some earlier EvtGen versions
-# will not be compatible with all external depedency versions given below, owing to C++
+# will not be compatible with all external dependency versions given below, owing to C++
 # interface differences; see the specific tagged version of the EvtGen/README file for guidance
 
-VERSION=R01-05-00
+# Version or tag number. No extraneous spaces on this line!
+VERSION=R01-04-00
 
 mkdir -p EvtGen
 cd EvtGen
@@ -25,27 +26,27 @@ cd external
 
 # Minimum required versions of the external packages. Later versions should be OK as well,
 # assuming their C++ interfaces do not change
-wget http://home.thep.lu.se/~torbjorn/pythia8/pythia8180.tgz
-wget http://photospp.web.cern.ch/photospp/resources/PHOTOS.3.54/PHOTOS.3.54.tar.gz
-wget http://tauolapp.web.cern.ch/tauolapp/resources/TAUOLA.1.1.4/TAUOLA.1.1.4.tar.gz
-wget http://lcgapp.cern.ch/project/simu/HepMC/download/HepMC-2.06.08.tar.gz
+wget http://home.thep.lu.se/~torbjorn/pythia8/pythia8186.tgz
+wget http://photospp.web.cern.ch/photospp/resources/PHOTOS.3.56/PHOTOS.3.56.tar.gz
+wget http://tauolapp.web.cern.ch/tauolapp/resources/TAUOLA.1.1.5/TAUOLA.1.1.5.tar.gz
+wget http://lcgapp.cern.ch/project/simu/HepMC/download/HepMC-2.06.09.tar.gz
 
 echo Extracting external dependencies
-tar -xzf PHOTOS.3.54.tar.gz 
-tar -xzf HepMC-2.06.08.tar.gz 
-tar -xzf pythia8180.tgz
-tar -xzf TAUOLA.1.1.4.tar.gz 
+tar -xzf PHOTOS.3.56.tar.gz 
+tar -xzf HepMC-2.06.09.tar.gz 
+tar -xzf pythia8186.tgz
+tar -xzf TAUOLA.1.1.5.tar.gz 
 
 echo Installing HepMC in $INSTALL_BASE/external/HepMC/
 mkdir -p HepMC
 cd HepMC/
-cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_BASE/external/HepMC/ $INSTALL_BASE/external/HepMC-2.06.08/ -Dmomentum:STRING=MEV -Dlength:STRING=MM
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_BASE/external/HepMC/ $INSTALL_BASE/external/HepMC-2.06.09/ -Dmomentum:STRING=GEV -Dlength:STRING=MM
 make
 make install
 
-echo Installing pythia8 in $INSTALL_BASE/external/pythia8180/
-cd ../pythia8180/
-./configure --with-hepmc=$INSTALL_BASE/external/HepMC/ --with-hepmcversion=2.06.08 --enable-shared
+echo Installing pythia8 in $INSTALL_BASE/external/pythia8186/
+cd ../pythia8186/
+./configure --with-hepmc=$INSTALL_BASE/external/HepMC/ --with-hepmcversion=2.06.09 --enable-shared
 make
 
 echo Installing TAUOLA in $INSTALL_BASE/external/TAUOLA/
@@ -60,11 +61,11 @@ make
 
 echo Building EvtGen
 cd $INSTALL_BASE/$VERSION
-./configure --hepmcdir=$INSTALL_BASE/external/HepMC/ --photosdir=$INSTALL_BASE/external/PHOTOS/ --pythiadir=$INSTALL_BASE/external/pythia8180/ --tauoladir=$INSTALL_BASE/external/TAUOLA/
+./configure --hepmcdir=$INSTALL_BASE/external/HepMC/ --photosdir=$INSTALL_BASE/external/PHOTOS/ --pythiadir=$INSTALL_BASE/external/pythia8186/ --tauoladir=$INSTALL_BASE/external/TAUOLA/
 make
 
 echo Setup done.
 echo To complete, add the following command to your .bashrc file or run it in your terminal before running any programs that use the EvtGen library:
-echo LD_LIBRARY_PATH=$INSTALL_BASE/external/HepMC/lib:$INSTALL_BASE/external/pythia8180/lib:$INSTALL_BASE/external/PHOTOS/lib:$INSTALL_BASE/external/TAUOLA/lib:$INSTALL_BASE/$VERSION/lib:\$LD_LIBRARY_PATH
+echo LD_LIBRARY_PATH=$INSTALL_BASE/external/HepMC/lib:$INSTALL_BASE/external/pythia8186/lib:$INSTALL_BASE/external/PHOTOS/lib:$INSTALL_BASE/external/TAUOLA/lib:$INSTALL_BASE/$VERSION/lib:\$LD_LIBRARY_PATH
 echo Also set the Pythia8 data path:
-echo PYTHIA8DATA=$INSTALL_BASE/external/pythia8180/xmldoc
+echo PYTHIA8DATA=$INSTALL_BASE/external/pythia8186/xmldoc
