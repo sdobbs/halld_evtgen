45e9a037df1e5a1ab868cff7da7c2a0d07848260
Thomas Latham <T.Latham@warwick.ac.uk>
2019-02-04 23:20:55 +0000

    Implement exporting of the lib targets in the generated config file

--- cmake/Modules/InstallPaths.cmake
+++ cmake/Modules/InstallPaths.cmake
@@ -3,6 +3,6 @@
 # except on frameworks where we use the system default
 set(CMAKE_INSTALL_BINDIR bin)
 set(CMAKE_INSTALL_LIBDIR lib)
-#set(CMAKE_INSTALL_INCLUDEDIR include)
+set(CMAKE_INSTALL_INCLUDEDIR include)
 #set(CMAKE_INSTALL_FRAMEWORKDIR /Library/Frameworks)
 
